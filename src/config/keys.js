0d2a2a09323dd57e6d4fc3b0333836d5a4fc14eb
Aaron-gg <uagg.296246@gmail.com>
2022-09-03 00:26:57 -0500

    direccion de servidor de pruevas agregada

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,9 @@
 module.exports = {
     mongodb: {
         URI: 'mongodb+srv://Zek:admin@cluster0.617fy.mongodb.net/test'
+        //URI: 'mongodb://localhost:27017'
     },
     secretKey: "Gusers",
-    url: "http://localhost:3333",
-    //url: "http://143.198.230.233:3333",
+    //url: "http://localhost:3333",
+    url: "http://165.227.199.243:3333",
 };
